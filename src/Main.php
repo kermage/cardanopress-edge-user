e35c271581da00c27c9c7d585af34322304ecdfe
kermage <genealyson.torcende@gmail.com>
2023-05-07 21:36:47 +0800

    filter transient to correctly point download link

--- src/Main.php
+++ src/Main.php
@@ -11,7 +11,22 @@ use EUM_Handler;
 
 class Main {
 
-	public const UPDATE_DATA_FORMAT = 'https://raw.githubusercontent.com/CardanoPress/%s/main/update-data.json';
+	public const UPDATE_DATA_FORMAT   = 'https://raw.githubusercontent.com/CardanoPress/%s/main/update-data.json';
+	public const DOWNLOAD_LINK_FORMAT = 'https://github.com/CardanoPress/%s/archive/refs/tags/v%s.zip';
+
+
+	protected function get_key( string $plugin ): string {
+
+		return 'eum_plugin_' . dirname( $plugin );
+
+	}
+
+
+	protected function get_slug( string $transient ): string {
+
+		return str_replace( 'eum_plugin_', '', $transient );
+
+	}
 
 
 	public function load(): void {
@@ -25,7 +40,32 @@ class Main {
 			$repo_name   = str_replace( 'cardanopress-', 'plugin-', dirname( $plugin ) );
 
 			EUM_Handler::run( wp_normalize_path( $plugin_path ), sprintf( self::UPDATE_DATA_FORMAT, $repo_name ) );
+			add_filter(
+				'pre_set_site_transient_' . $this->get_key( $plugin ),
+				array( $this, 'filter_transient' ),
+				10,
+				2
+			);
 		}
+
+	}
+
+
+	/**
+	 * @param mixed $value
+	 * @param string $transient
+	 *
+	 * @return mixed
+	 */
+	public function filter_transient( $value, string $transient ) {
+
+		if ( empty( $value ) || ! is_object( $value ) || ! isset( $value->download_link, $value->new_version ) ) {
+			return $value;
+		}
+
+		$value->download_link = sprintf( self::DOWNLOAD_LINK_FORMAT, $this->get_slug( $transient ), $value->new_version );
+
+		return $value;
 
 	}
 
