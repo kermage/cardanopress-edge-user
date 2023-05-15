8905ed5d0f11d85ede553a15cbb1e111c6c14f23
kermage <genealyson.torcende@gmail.com>
2023-05-15 23:01:17 +0800

    we need the vendor files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kermage/cardanopress-edge-user',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8e1ed0a0ba13e9a39836a362d799d9cd8d4fb4e3',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'kermage/cardanopress-edge-user' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8e1ed0a0ba13e9a39836a362d799d9cd8d4fb4e3',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'kermage/external-update-manager' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => '00506d4e76047026d0c19f39223a8f2051593686',
+            'type' => 'wordpress-dropin',
+            'install_path' => __DIR__ . '/../kermage/external-update-manager',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
